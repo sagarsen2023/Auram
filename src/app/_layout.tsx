047755ba2b87b0096d8bed4a9a2971c1f4743c04
Sagar Sen <sagarsen.dev@gmail.com>
2024-10-05 12:42:44 +0530

    Refactored the code for getting started to building the welcome screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,17 @@
 import * as SplashScreen from "expo-splash-screen";
-import {Stack} from "expo-router";
+import { Stack } from "expo-router";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    return (
-        <Stack>
-            <Stack.Screen
-                name="(welcome)/index"
-                options={{
-                    headerShown: false,
-                }}
-            />
-        </Stack>
-    );
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(welcome)/index"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 }
