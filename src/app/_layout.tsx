0b9bf5621363625f092189346d256192ad768ed1
Sagar Sen <sagarsen.dev@gmail.com>
2024-10-05 12:37:13 +0530

    Updated main layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,17 @@
 import * as SplashScreen from "expo-splash-screen";
-import { Stack } from "expo-router";
+import {Stack} from "expo-router";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+    return (
+        <Stack>
+            <Stack.Screen
+                name="(welcome)/index"
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Stack>
+    );
 }
