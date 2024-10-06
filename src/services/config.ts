c445efb50eee2026f60c768490e116cf503840bc
Sagar Sen <sagarsen.dev@gmail.com>
2024-10-06 22:51:30 +0530

    Add @react-native-async-storage/async-storage and react-native-toast-message dependencies

---
+++ src/services/config.ts
@@ -0,0 +1,44 @@
+import { BASE_URL } from "./queryUrls";
+
+interface APIOptions extends RequestInit {
+  headers?: Record<string, string>;
+}
+
+
+async function request<T>(
+  method: string,
+  endpoint: string,
+  options: APIOptions = {},
+): Promise<T> {
+  const url = `${BASE_URL}${endpoint}`;
+
+  const response = await fetch(url, {
+    ...options,
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      // ...(token && { Authorization: `Bearer ${token}` }),
+      // ...(seller && { Seller: seller }),
+      // ...(buyer && { Buyer: buyer }),
+
+      ...options.headers,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('API request failed');
+  }
+
+  return response.json();
+}
+
+export const fetchAPI = {
+  get: <T>(endpoint: string, options: APIOptions = {}) =>
+    request<T>('GET', endpoint, options),
+  post: <T>(endpoint: string, body: any, options: APIOptions = {}) =>
+    request<T>('POST', endpoint, { ...options, body: JSON.stringify(body) }),
+  put: <T>(endpoint: string, body: any, options: APIOptions = {}) =>
+    request<T>('PUT', endpoint, { ...options, body: JSON.stringify(body) }),
+  delete: <T>(endpoint: string, options: APIOptions = {}) =>
+    request<T>('DELETE', endpoint, options),
+};
---
+++ src/services/queryUrls.ts
@@ -0,0 +1,6 @@
+// BASE URL
+export const BASE_URL = "https://auram.setside.app/api"
+
+// AUTHENTICATION URLS
+export const REGISTRATION_URL = "/auth/register"
+export const LOGIN_URL = "/auth/login"
