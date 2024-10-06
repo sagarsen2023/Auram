ac03ce531666798f4d8048ad117d14c3852df9f3
Sagar Sen <sagarsen.dev@gmail.com>
2024-10-06 11:13:42 +0530

    Added onboarding screen

--- src/app/(welcome)/_layout.tsx
+++ src/app/(welcome)/_layout.tsx
@@ -2,9 +2,15 @@ import { Stack } from "expo-router";
 
 export default function _layout() {
   return (
-    <Stack>
+    <Stack initialRouteName="index">
       <Stack.Screen
         name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="onboarding"
         options={{
           headerShown: false,
         }}
