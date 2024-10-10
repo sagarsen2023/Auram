ba59d394bbaaef65b071e69288fae5f3021d9236
Sagar Sen <sagarsen.dev@gmail.com>
2024-10-10 09:48:49 +0530

    Added margin and padding sizes in theme constant file

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,16 +1,24 @@
 import { useColorScheme } from "react-native";
 
 export const useThemeColor = () => {
-    const colorScheme = useColorScheme();
-    const COLORS = {
-        primary: colorScheme === "light" ? "#704F38" : "#A67D5D",
-        secondary: colorScheme === "light" ? "white" : "black",
-        text: colorScheme === "light" ? "black" : "white",
-    }
-    return COLORS;
-}
+  const colorScheme = useColorScheme();
+  const COLORS = {
+    primary: colorScheme === "light" ? "#704F38" : "#A67D5D",
+    secondary: colorScheme === "light" ? "white" : "black",
+    text: colorScheme === "light" ? "black" : "white",
+  };
+  return COLORS;
+};
 
-export const SIZES = {};
+export const SIZES = {
+  marginOrPadding: {
+    xSmall: 4,
+    small: 8,
+    medium: 16,
+    large: 24,
+    xLarge: 32,
+  },
+};
 
 export const TYPOGRAPHY = {
   h1: {
