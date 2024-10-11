7d29966847ee78f085a1344c0f4561981177b20e
Sagar Sen <sagarsen.dev@gmail.com>
2024-10-11 11:57:17 +0530

    Added a basic page to perform authorization

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router";
 
+
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
--- src/hooks/token.ts
+++ src/hooks/token.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export async function getToken() {
+export async function getToken(): Promise<string | null> {
   return await AsyncStorage.getItem("bearerToken");
 }
 
