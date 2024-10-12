2a0257d5a9f9f3f2f469ce5e80f9b9e3e39a0a5d
Sagar Sen <sagarsen.dev@gmail.com>
2024-10-12 20:14:43 +0530

    Added image base url

--- src/services/queryUrls.ts
+++ src/services/queryUrls.ts
@@ -1,5 +1,6 @@
 // BASE URL
 export const BASE_URL = "https://auram.setside.app/api";
+export const IMAGE_URL = "https://pub-d7fc7e0204644a99b00264cba5e82690.r2.dev";
 
 // AUTHENTICATION URLS
 export const REGISTRATION_URL = "/auth/register";
