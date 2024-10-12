c5252f7f6c452b3c5510cc81184ef99396649cfd
Sagar Sen <sagarsen.dev@gmail.com>
2024-10-12 10:47:45 +0530

    Added tab navigation

--- src/app/(root)/_layout.tsx
+++ src/app/(root)/_layout.tsx
@@ -1,29 +1,125 @@
+import { ReactNode } from "react";
+import { useThemeColor } from "@/src/constants/theme";
 import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Entypo from "@expo/vector-icons/Entypo";
+import { View } from "react-native";
+
 export default function _layout() {
+  const COLORS = useThemeColor();
+
+  const TabBarIcon = ({
+    focused,
+    icon,
+  }: {
+    focused: boolean;
+    icon: ReactNode;
+  }) => {
+    return (
+      <View
+        style={{
+          backgroundColor: focused ? COLORS.primary : "transparent",
+          width: 55,
+          height: 55,
+          borderRadius: 50,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {icon}
+      </View>
+    );
+  };
+
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.secondary,
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 20,
+          overflow: "hidden",
+          paddingBottom: 0,
+          paddingHorizontal: 0,
+          paddingTop: 0,
+          height: 70,
+        },
+      }}
+    >
       <Tabs.Screen
         name="(home)"
         options={{
           title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              focused={focused}
+              icon={
+                <Entypo
+                  name="home"
+                  size={24}
+                  color={focused ? "white" : COLORS.text}
+                />
+              }
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="(cart)"
         options={{
           title: "Cart",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              focused={focused}
+              icon={
+                <Entypo
+                  name="shopping-cart"
+                  size={24}
+                  color={focused ? "white" : COLORS.text}
+                />
+              }
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="wishlist"
         options={{
           title: "Wishlist",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              focused={focused}
+              icon={
+                <AntDesign
+                  name="heart"
+                  size={24}
+                  color={focused ? "white" : COLORS.text}
+                />
+              }
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              focused={focused}
+              icon={
+                <FontAwesome
+                  name="user"
+                  size={24}
+                  color={focused ? "white" : COLORS.text}
+                />
+              }
+            />
+          ),
         }}
       />
     </Tabs>
