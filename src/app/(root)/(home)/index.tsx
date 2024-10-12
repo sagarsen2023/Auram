15029a0212f0fe2e78d6624fddd23a39adb46ebb
Sagar Sen <sagarsen.dev@gmail.com>
2024-10-12 20:16:04 +0530

    Added banner component with API calls

--- src/app/(root)/(home)/index.tsx
+++ src/app/(root)/(home)/index.tsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import Banner from "@/src/components/home-components/banner.component";
+import { BannerData } from "@/src/models/banner.model";
+import homeAPI from "@/src/services/home.service";
+import { SIZES, useThemeColor } from "@/src/constants/theme";
 
 const Home = () => {
+  const COLORS = useThemeColor();
+  const [bannerData, setBannerData] = useState<BannerData[] | null>(null);
+
+  useEffect(() => {
+    const fetchBannerData = async () => {
+      const response = await homeAPI.fetchBanner();
+      if (response.status) setBannerData(response.data);
+    };
+    fetchBannerData();
+  }, []);
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: COLORS.secondary,
+        },
+      ]}
+    >
+      <ScrollView>
+        <Banner bannerData={bannerData} />
+      </ScrollView>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: SIZES.marginOrPadding.small
+  },
+});
