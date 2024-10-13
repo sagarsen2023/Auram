aa2241028d4d713535a3b16f505af6f4c4524e81
Sagar Sen <sagarsen.dev@gmail.com>
2024-10-13 12:27:19 +0530

    Completed category listing

--- src/components/cards/category-rounded-card.component.tsx
+++ src/components/cards/category-rounded-card.component.tsx
@@ -1,14 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, View } from "react-native";
+import React from "react";
+import { CategoryItem } from "@/src/models/categories-and-items/category.model";
+import { SIZES, useThemeColor } from "@/src/constants/theme";
+import imageValidator from "@/src/utils/imageValidator";
+import ThemeText from "../theme-text.component";
 
-const CategoryRoundedCard = () => {
+const CategoryRoundedCard = ({ category }: { category: CategoryItem }) => {
+  const COLORS = useThemeColor();
   return (
-    <View>
-      <Text>CategoryRoundedCard</Text>
+    <View style={styles.wrapper}>
+      <View
+        style={[
+          styles.imageContainer,
+          {
+            backgroundColor: COLORS.primaryLite,
+          },
+        ]}
+      >
+        <Image
+          style={styles.imageStyles}
+          source={imageValidator(category.media.path)}
+          resizeMode="cover"
+        />
+      </View>
+      <ThemeText style={styles.title}>
+        {category.title.length > 10
+          ? `${category.title.slice(0, 10)}...`
+          : category.title}
+      </ThemeText>
     </View>
-  )
-}
+  );
+};
 
-export default CategoryRoundedCard
+export default CategoryRoundedCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  wrapper: {
+    marginRight: SIZES.marginOrPadding.default,
+    gap: SIZES.marginOrPadding.xSmall,
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+    padding: SIZES.marginOrPadding.xSmall,
+  },
+  imageStyles: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 50,
+  },
+  title: {
+    fontWeight: "500",
+  },
+});
--- src/components/home-components/category-list.component.tsx
+++ src/components/home-components/category-list.component.tsx
@@ -1,19 +1,74 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import React from "react";
 import { CategoryItem } from "@/src/models/categories-and-items/category.model";
+import CategoryRoundedCard from "../cards/category-rounded-card.component";
+import ThemeText from "../theme-text.component";
+import { SIZES, useThemeColor } from "@/src/constants/theme";
+
+// TODO: Implement onPress for category
 
 const CategoryList = ({
   categories,
 }: {
   categories: CategoryItem[] | null;
 }) => {
+  const COLORS = useThemeColor();
   return (
     <View>
-      <Text>CategoryList</Text>
+      {categories && (
+        <View style={styles.container}>
+          <View style={styles.textContainer}>
+            <ThemeText style={styles.headingStyle}>Categories</ThemeText>
+            <ThemeText
+              style={{
+                color: COLORS.primary,
+                fontWeight: "bold",
+              }}
+            >
+              See All
+            </ThemeText>
+          </View>
+
+          <FlatList
+            data={categories}
+            keyExtractor={(item) => item._id}
+            renderItem={(item) => (
+              <TouchableWithoutFeedback
+                onPress={() => {
+                  console.log("Category Clicked");
+                }}
+              >
+                <CategoryRoundedCard category={item.item} />
+              </TouchableWithoutFeedback>
+            )}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
+      )}
     </View>
   );
 };
 
 export default CategoryList;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: SIZES.marginOrPadding.default,
+  },
+  headingStyle: {
+    fontSize: SIZES.fontSize.large,
+    fontWeight: "bold",
+  },
+  textContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: SIZES.marginOrPadding.medium,
+  },
+});
