7cf14e2b77721b191133772af655db47f466f3e3
Sagar Sen <sagarsen.dev@gmail.com>
2024-11-03 10:11:30 +0530

    Added shimmer effect for main product card

---
+++ src/components/shimmer-loaders/main-product-card-loader.component.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { SIZES } from "@/src/constants/theme";
+import { ShimmerContainer } from "./shimmer-effect-container.component";
+
+const MainProductCardLoader = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ShimmerContainer borderRadius={SIZES.borderRadius.default} />
+      </View>
+
+      <View style={styles.contentContainer}>
+        <ShimmerContainer width="90%" height={20} />
+        <ShimmerContainer
+          width="60%"
+          height={16}
+          style={styles.pricePlaceholder}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "48%",
+    marginBottom: SIZES.marginOrPadding.default,
+  },
+  imageContainer: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  contentContainer: {
+    marginTop: SIZES.marginOrPadding.small,
+  },
+  pricePlaceholder: {
+    marginTop: SIZES.marginOrPadding.xSmall,
+  },
+});
+
+export default MainProductCardLoader;
